d87b589c3eaa2492eea0fbde087e6c8c7545db6f
michaeloye <michaeloye23@gmail.com>
2022-01-14 23:58:09 +0100

    Add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "base-purple": "#6a61e6",
+      },
+    },
   },
   plugins: [],
-}
+};
