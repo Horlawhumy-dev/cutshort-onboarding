50033efaf1b416bbb316943c23349d6ff5b8c51f
michaeloye <michaeloye23@gmail.com>
2022-01-15 16:04:15 +0100

    Update text in p tag

--- src/pages/howToUse.jsx
+++ src/pages/howToUse.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import NextButton from "../components/nextButton";
-import UseCard from "../components/useCard";
+import PlanCard from "../components/planCard";
 
 function HowToUse() {
   const [focusCardOne, setFocusCardOne] = useState();
@@ -17,21 +17,21 @@ function HowToUse() {
   return (
     <div className="mt-10">
       <p className="text-lg font-medium lg:text-2xl text-text-black">
-        Let's set up a home for all your work
+        How are you planning to use Onboarding?
       </p>
       <p className="mt-2 text-xs lg:text-sm font-light text-gray-400">
-        You can always create another workspace later
+        We'll streamline your setup experience accordingly
       </p>
       <div className="flex flex-col items-center">
         <div className="flex justify-center gap-7 mt-7 w-3/4 md:w-6/12 lg:w-3/12">
-          <UseCard
+          <PlanCard
             icon="FaUser"
             option="For myself"
             description="Write better. Think more clearly. Stay organized."
             focus={focusCardOne}
             onFocus={onFocusOne}
           />
-          <UseCard
+          <PlanCard
             icon="RiTeamFill"
             option="With my team"
             description="Wikis, docs, tasks & projects, all in one place."
