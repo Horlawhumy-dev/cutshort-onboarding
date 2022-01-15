f4e9145b029a3a6c0a1337a2596f54718148adb1
michaeloye <michaeloye23@gmail.com>
2022-01-15 13:46:41 +0100

    Add link props

--- src/components/nextButton.jsx
+++ src/components/nextButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function NextButton({ tag }) {
+function NextButton({ tag, link }) {
   return (
     <div className="mt-8 w-3/4 md:w-6/12 lg:w-3/12">
-      <Link to="/workspace">
+      <Link to={link}>
         <button className="py-2 w-full bg-base-purple text-white rounded">
           {tag}
         </button>
