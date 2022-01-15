4beac2b5d8ffa4642b5455fdc5570632b312fcc5
michaeloye <michaeloye23@gmail.com>
2022-01-15 09:02:46 +0100

    Updata text color

--- src/components/number.jsx
+++ src/components/number.jsx
@@ -4,9 +4,9 @@ function Number({ number, active }) {
   return (
     <div
       className={
-        "rounded-full bg-base-purple flex justify-center items-center h-7 w-7 lg:h-9 lg:w-9 text-white text-sm " +
+        "rounded-full bg-base-purple flex justify-center items-center h-7 w-7 lg:h-9 lg:w-9 text-sm " +
         (active
-          ? "bg-base-purple"
+          ? "bg-base-purple text-white "
           : "bg-white text-gray-700 border border-gray-300")
       }
     >
