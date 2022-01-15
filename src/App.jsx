eb513f6bba396e80cad9e2843fff6ea8e61e882d
michaeloye <michaeloye23@gmail.com>
2022-01-15 02:34:40 +0100

    Add header to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState} from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginPage from './components/login';
-import SignupPage from './components/signup';
-import  Loader from './components/Loader/loader';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/login";
+import SignupPage from "./components/signup";
+import Loader from "./components/Loader/loader";
+import Header from "./components/header";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -14,20 +15,21 @@ function App() {
   }, []);
   return (
     <div className="App">
-    {isLoading ? (
-      <Loader />
-    ) : (
-      <>
-        <Routes>
-          <Route exact path="/signin" element={<LoginPage />} />
-          <Route exact path="/signup" element={<SignupPage />} />
-        </Routes>
-      </>
-    )}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route exact path="/signin" element={<LoginPage />} />
+            <Route exact path="/signup" element={<SignupPage />} />
+          </Routes>
+        </>
+      )}
     </div>
-  )
+  );
 }
- const WithRouter = () => {
+const WithRouter = () => {
   return (
     <div>
       <BrowserRouter>
@@ -36,4 +38,4 @@ function App() {
     </div>
   );
 };
-export default WithRouter
+export default WithRouter;
